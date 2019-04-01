7e809a3fbd6a7ece49cd258b14273e7d854d2d71
hf-hf <553527481@qq.com>
2019-04-01 16:45:56 +0800

    学习提交

---
+++ Day1/多个变量赋值.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+标准数据类型
+Python3 中有六个标准的数据类型：
+
+Number（数字）
+String（字符串）
+List（列表）
+Tuple（元组）
+Set（集合）
+Dictionary（字典）
+
+Python3 的六个标准数据类型中：
+
+不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）；
+可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）。
+"""
+
+a = b = c = 1
+print(a)
+print(b)
+print(c)
+
+a, b, c = 1, 2, "runoob"
+print(a)
+print(b)
+print(c)
